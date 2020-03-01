d5be8083c068241734a8fe44601753796699d04e
soyoung <soyoungp02@gmail.com>
2020-03-01 23:42:04 +0900

    install package & write test code

--- tests/App-test.js
+++ tests/App-test.js
@@ -5,10 +5,11 @@
 import 'react-native';
 import React from 'react';
 import App from '../App';
+import { Item } from 'react-native/Libraries/Components/Picker/Picker';
 
-// Note: test renderer must be required after react-native.
-import renderer from 'react-test-renderer';
-
-it('renders correctly', () => {
-  renderer.create(<App />);
-});
+describe('Jest', () => {
+  it('is it working?', () => {
+    const a = 1;
+    expect(a + 1).toBe(2);
+  })
+})
