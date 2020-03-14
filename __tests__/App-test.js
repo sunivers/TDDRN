7c479fb171efd18eb63638089f0edd7f64d86a3e
soyoung <soyoungp02@gmail.com>
2020-03-15 02:44:15 +0900

    프로젝트 재정리, jest 동작 확인, Enzyme 설치 및 동작 확인

--- __tests__/App-test.js
+++ __tests__/App-test.js
@@ -6,9 +6,20 @@ import 'react-native';
 import React from 'react';
 import App from '../App';
 
-// Note: test renderer must be required after react-native.
-import renderer from 'react-test-renderer';
+import {Text} from 'react-native';
+import {shallow} from 'enzyme';
 
-it('renders correctly', () => {
-  renderer.create(<App />);
+describe('Jest', () => {
+  it('is it working?', () => {
+    const a = 1;
+    expect(a + 1).toBe(2);
+  });
+});
+
+describe('Enzyme', () => {
+  it('is it working?', () => {
+    const text = 'some text';
+    const wrapper = shallow(<Text>{text}</Text>);
+    expect(wrapper.text()).toBe(text);
+  });
 });
--- jest.config.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-  preset: 'react-native',
-  testMatch: ['**/tests/**/*test.[jt]s?(x)'],
-  setupFiles: ['./tests/setup.js'],
-};
